401ac70b0f712a9aa5de1ee3d119f53ff672e411
Joshua Lamson <jlamson@mymail.mines.edu>
2013-01-27 20:26:27 -0700

    Added hooks in for /form and /list. printing text

--- project2/albums.rb
+++ project2/albums.rb
@@ -1,9 +1,26 @@
 require 'rack'
 
 class HelloWorld
-  def call(env)
-    [200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
-  end
+	def call(env)
+		request = Rack::Request.new(env)
+
+		case request.path
+		when "/form" then generate_form()
+		when "/list" then generate_list()
+		else [404, {"Content-Type" => "text/plain"}, ["OH SHIT!\n"]]
+		end
+	end
+
+	def generate_form()
+		[200, {"Content-Type" => "text/plain"}, ["form"]]	
+	end
+
+	def generate_list()
+		[200, {"Content-Type" => "text/plain"}, ["list"]]
+	end
 end
 
+Signal.trap('INT') {
+  Rack::Handler::WEBrick.shutdown
+}
 Rack::Handler::WEBrick.run HelloWorld.new, :Port => 8080

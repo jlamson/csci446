023c56c3a3219f1d66aaf8cd1061d2812d894386
Joshua Lamson <jlamson@mymail.mines.edu>
2013-01-27 20:51:54 -0700

    parsed html page with simple title and content variables

--- project2/albums.rb
+++ project2/albums.rb
@@ -1,25 +1,35 @@
 require 'rack'
+require 'erb'
 
 class HelloWorld
 	def call(env)
 		request = Rack::Request.new(env)
 
 		case request.path
-		when "/form" then generate_form()
-		when "/list" then generate_list()
+		when "/form" then generate_form(request)
+		when "/list" then generate_list(request)
 		else [404, {"Content-Type" => "text/plain"}, ["OH SHIT!\n"]]
 		end
 	end
 
-	def generate_form()
-		file = File.open("form.html", "rb")
-		page = file.read
-		file.close
-		[200, {"Content-Type" => "text/html"}, [page]]	
+	def generate_form(request)
+		page = get_page("form.html")
+		title = "foo"
+		content = "blah"
+		erb = ERB.new(page).result(binding)
+		[200, {"Content-Type" => "text/html"}, [erb]]	
 	end
 
-	def generate_list()
+	def generate_list(request)
 		[200, {"Content-Type" => "text/plain"}, ["list"]]
+	end
+
+	private
+	def get_page(path)
+		file = File.open(path, "rb")
+		page = file.read
+		file.close
+		page
 	end
 end
 

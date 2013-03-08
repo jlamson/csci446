c1145ffa9d13db908b5fc5e4dfbcbd04bf0148db
Joshua Lamson <jlamson@mymail.mines.edu>
2013-03-07 21:53:50 -0700

    Added method to cart to add a product via a line_item. If line_item for that product exists, just add 1 to quantity

--- project04/app/models/cart.rb
+++ project04/app/models/cart.rb
@@ -2,4 +2,15 @@ class Cart < ActiveRecord::Base
   # attr_accessible :title, :body
 
   has_many :line_items, dependent: :destroy
+
+  def add_product(product_id)
+    current_item = line_items.where(product: product.id).first
+    if current_item
+      current_item.quantity += 1
+    else 
+      currrent_item = line_items.build(product_id: product_id)
+    end
+    current_item
+  end
+  
 end

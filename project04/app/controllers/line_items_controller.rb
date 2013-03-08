d73b239f85053036bd0960e87c0cfe01eff434eb
Joshua Lamson <jlamson@mymail.mines.edu>
2013-03-07 23:56:38 -0700

    Chapter 10 completed. Created a new cart that condenses items with multiple orders. Also made the cart look nice, and handeled some errors

--- project04/app/controllers/line_items_controller.rb
+++ project04/app/controllers/line_items_controller.rb
@@ -42,8 +42,7 @@ class LineItemsController < ApplicationController
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build
-    @line_item.product = product
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
@@ -79,7 +78,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url }
+      format.html { redirect_to @line_item.cart }
       format.json { head :no_content }
     end
   end

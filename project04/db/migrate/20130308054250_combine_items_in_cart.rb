1063db70f44549ec77383e3ea36111e709fb5bbd
Joshua Lamson <jlamson@mymail.mines.edu>
2013-03-07 22:50:22 -0700

    created up method to condense multiple rows, and migrated

--- project04/db/migrate/20130308054250_combine_items_in_cart.rb
+++ project04/db/migrate/20130308054250_combine_items_in_cart.rb
@@ -1,5 +1,13 @@
 class CombineItemsInCart < ActiveRecord::Migration
   def up
+    Cart.all.each do |cart|
+      cart.line_items.group(:product_id).sum(:quantity).each do |product_id, quantity|
+        if quantity > 1
+          cart.line_items.where(product_id: product_id).delete_all
+          cart.line_items.create(product_id: product_id, quantity: quantity)
+        end
+      end  
+    end
   end
 
   def down
